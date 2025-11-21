9307d9b6ed2324236e4fac7c179cc4e4d5f6188c
Omar Akhtar <oakhtar@d2l.com>
2025-11-20 23:29:27 -0500

    Update python_datahub_dataset_relationships_v124.py

--- python_datahub_dataset_relationships_v124.py
+++ python_datahub_dataset_relationships_v124.py
@@ -224,7 +224,7 @@ def main():
     with st.expander("❓ How to Use This Application", expanded=False):
         st.markdown("""
         ### 1. Get Started
-        Use the **Sidebar** on the left to select datasets. If you haven't loaded data yet, expand "STEP 1" and click the Scrape button; when net new Dataset **Categories** are created, they will have their own URL which will need to be added in the "URLs to Scrape" field.
+        Use the **Sidebar** on the left to select datasets. If you haven't loaded data yet, expand "STEP 1" and click the Scrape button; when net new Dataset **Categories** are created, they will have their own URL which will need to be added in the "URLs to Scrape" field (which the code maintainer will attempt to update and...maintain~)
 
         ### 2. Graph Modes
         *   **Focused Mode:** best for checking how two specific tables join together.
@@ -372,6 +372,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
